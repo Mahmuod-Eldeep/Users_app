a88491ff9e0bfb5076ae08ac085df3bdaeebc85c
AMhmoud <you@example.com>
2024-02-15 22:56:34 +0200

    Added all files project

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$con = new mysqli('localhost', 'root', '', 'user_app');
+
+
+if (!$con) {
+    die(mysqli_error($con));
+}
---
+++ js/main.js
@@ -0,0 +1,110 @@
+function show_add() {
+
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    Command: toastr["info"]("user-add-successfully", "Add User")
+
+
+
+}
+function show_del() {
+
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    Command: toastr["error"]("user-delete-successfully", "delete User")
+
+
+
+}
+
+
+function show_update() {
+
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+
+    Command: toastr["success"]("user-updte-successfully", "Update User")
+
+
+
+}
+
+
+
+
+
+function confirm_delete(id) {
+
+
+    let del = confirm("Do you want to delete the user ?");
+    console.log(del)
+    if (del == true) {
+
+        window.location.href = "index.php?action=del&&id=" + id;
+
+    }
+}
+
+
+
+
+function edit(id) {
+
+
+
+
+    window.location.href = "add_user.php?action=edit&&id=" + id;
+
+
+}
+
+
